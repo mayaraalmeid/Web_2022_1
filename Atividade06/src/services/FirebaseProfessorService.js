86fb0e254c0cbadbea115f4c16b410a7e15518db
Mayara Almeida <69010544+mayaraalmeid@users.noreply.github.com>
2022-06-10 15:04:53 -0300

    Atividade 6

--- Atividade06/src/services/FirebaseProfessorService.js
+++ Atividade06/src/services/FirebaseProfessorService.js
@@ -1,6 +1,6 @@
 import { collection, documentId, doc, addDoc, deleteDoc, updateDoc, getDoc, getDocs, onSnapshot, query } from "firebase/firestore";
 
-export default class FirebaseStudentService {
+export default class FirebaseProfeossorService {
 
     static list = (firestore, callback) => {
         const coll = collection(firestore, 'professors')
